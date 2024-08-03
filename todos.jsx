b9b17b34efb8d496e37f0e1b28b0c2b9856d9d7b
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-08-03 07:09:29 +0530

    Create todos.jsx

---
+++ todos.jsx
@@ -0,0 +1,14 @@
+
+
+//imagine it like a databse where the todos are stored
+//we made it hard coded 
+
+export const TODOS = [{
+    id : 1,
+    title : "go to gym",
+    description : "hit the gym from 7-9",
+} , {
+    id : 2,
+    title : "eat food",
+    description : "eat food from 9 to 9 30"
+}]
